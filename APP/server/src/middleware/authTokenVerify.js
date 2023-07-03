4c27c3dd263b5bc6741360fab5d4bb5e75db0390
Batuhan <92050912+Cysquatch999@users.noreply.github.com>
2023-07-03 14:46:58 +0300

    Update authTokenVerify.js
    authTokenVerify.js updated.

--- APP/server/src/middleware/authTokenVerify.js
+++ APP/server/src/middleware/authTokenVerify.js
@@ -5,7 +5,7 @@ const verifyUserToken = (req, reply, next) => {
     if (!req.headers.authorization) {
         throw createError(401, "Unauthorized request. " + error);
     }
-    const token = req.headers["authorization"].split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];
     if (!token) {
         throw createError(401, "Access denied. No token provided. " + error);
     }
